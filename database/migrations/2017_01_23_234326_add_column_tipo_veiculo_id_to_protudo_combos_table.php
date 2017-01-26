26cf5dd47e198ba5692138723141b386b34bcb9e
Uriel Oliveira <uriel.f@icloud.com>
2017-01-25 23:09:54 -0200

    Finalizacao das configuracaos de motos

--- database/migrations/2017_01_23_234326_add_column_tipo_veiculo_id_to_protudo_combos_table.php
+++ database/migrations/2017_01_23_234326_add_column_tipo_veiculo_id_to_protudo_combos_table.php
@@ -14,8 +14,6 @@ class AddColumnTipoVeiculoIdToProtudoCombosTable extends Migration
     {
         Schema::table('produtos_combos', function (Blueprint $table) {
           $table->integer('tipo_veiculo_id')->default(1);
-          $table->dropPrimary('idprodutomaster');
-          $table->dropPrimary('idprodutoopcional');
         });
     }
 
